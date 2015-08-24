5be409c8d813f252fc0247cfb5b0ac6157faa938
Cian Booth <thisiscian@gmail.com>
2015-08-24 05:59:11 +0000

    added lots of updates, bot now works(?)

--- README.md
+++ README.md
@@ -7,3 +7,14 @@ The file 'consumer.txt' must contain two lines, containing the consumer secret a
 
 Similarly, 'access.txt' must contain two lines, containing the access token and key.
 
+A 'reponses.txt' file should also be populated with strings that python can format. 
+Valid labels are "Win","win","Draw","draw","Lose","lose" and "percentage".
+Title case labels give title case examples, e.g. Rock, Paper, Scissors, similarly for lower case labels.
+The percentage label returns a number between 0 and 100.
+
+For example:
+```python
+	I win, with {win}. ## I win, with rock.
+	{Lose} will always lose. ## Paper will always lose.
+	There is a {percentage}% chance of me choosing {draw}. ## There is a 13% chance of me choosing scissors.
+```
--- start_bot.py
+++ start_bot.py
@@ -1,22 +1,50 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3.4
 
+import sys
 from easy_tweepy import EasyTweepy
+from tweepy.error import TweepError
 import time
-from random import choice
-
+from random import choice, randrange
+import re
 wait=10
-
+response_file='responses.txt'
+weapons=['scissors','paper','rock',]
 with EasyTweepy() as ez:
 	ez.log('wait time is', wait, 'seconds')
 	while(True):
-		tweets=ez.mentions()
+		try:
+			tweets=ez.mentions()
+		except:
+			ez.log('Error: couldn\'t retrieve tweets')
+			raise
 		for tweet in tweets:
-			if 'rock' in tweet.text.lower(): response_file='paper.txt'
-			elif 'paper' in tweet.text.lower(): response_file='scissors.txt'
-			elif 'scissors' in tweet.text.lower(): response_file='rock.txt'
-			else: continue
-		
+			weapon=re.findall('('+'|'.join(weapons)+')',tweet.text.lower())
+			if len(weapon) == 0:
+				continue
+			if len(weapon) > 1:
+				ez.reply(tweet,'Ow, my logics.')
+				continue
+			weapon=weapon[0]
+			win_weapon=weapons[weapons.index(weapon)-1]
+			lose_weapon=weapons[weapons.index(weapon)-2]
 			with open(response_file) as responses:
-				response=choice(responses.read().splitlines())
-				ez.reply(tweet,response)
+				blank_response=choice([r for r in responses.read().splitlines() if r ])
+				response=blank_response.format(Win=win_weapon.title(), 
+				                               win=win_weapon, 
+				                               Draw=weapon.title(),
+				                               draw=weapon, 
+				                               Lose=lose_weapon.title(),
+				                               lose=lose_weapon,
+				                               percentage=randrange(100))
+				try:
+					ez.reply(tweet,response)
+				except TweepError as e:
+					if e.args[0]=='Twitter error response: status code = 403':
+						pass
+					else:
+						raise
+				except:
+					ez.log('Error: couldn\'t send message "'+response+'"')
+					raise
+			sys.stdout.flush()
 		time.sleep(wait)
